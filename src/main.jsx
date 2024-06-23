9d6f43cfed471dea18f17895d5557c1b743385c8
lurch <alexhann17@yahoo.com>
2024-06-22 19:10:34 -1000

    removed unnecessary UserContext

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import UserProvider from './context/UserContext';
 import AuthProvider from './context/AuthContext';
 
 import { CartProvider } from './context/CartContext';
@@ -12,13 +11,11 @@ import { CartProvider } from './context/CartContext';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <UserProvider>
           <CartProvider>
           <BrowserRouter>
             <App />
           </BrowserRouter>
           </CartProvider>
-      </UserProvider>
     </AuthProvider>
   </React.StrictMode>,
 );
